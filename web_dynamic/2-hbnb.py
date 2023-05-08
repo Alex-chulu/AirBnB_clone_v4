8f665d46582b4aa327e5a80af87b2a7ac3a85ed6
Alex-Chulu <chulualexandar@gmail.com>
2023-05-08 00:08:28 +0000

    API status

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+"""
+from flask import Flask, render_template
+from os import environ
+app = Flask(__name__)
+
+
+@app.route('/2-hbnb')
+def display_hbnb():
+    """
+    Display HBNB home page
+    """
+    return render_template('2-hbnb.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=environ.get('HBNB_API_PORT', 5000))
+
